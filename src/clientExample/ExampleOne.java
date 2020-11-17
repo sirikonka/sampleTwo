ae8b6961b7a13ad904b5373398deea1610af57a8
sirikonka <61365613+sirikonka@users.noreply.github.com>
2020-11-17 11:53:59 -0800

    adding one more method

--- src/clientExample/ExampleOne.java
+++ src/clientExample/ExampleOne.java
@@ -4,5 +4,8 @@ public class ExampleOne {
     public static void main(String[] args) {
         System.out.println("This is my sample one");
     }
+    public void aFunctionality(){
+        System.out.println("Just added one more method");
+    }
 
 }
